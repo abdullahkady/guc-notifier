1dd34aa11041b3d3c60f984eaff59e727e1faeb0
Abdullah Kady <abdullahkady@gmail.com>
2018-11-28 09:28:12 +0200

    Add proper environment loading

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+/*
+  This module just initalizes the environment variables.
+  To work with es6 module, check: https://github.com/motdotla/dotenv/issues/133#issuecomment-255298822
+*/
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { PORT, MONGO_URI } = process.env;
+export { PORT, MONGO_URI };
--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("I'm a server ! :D");
+import mongoose from 'mongoose';
+import { MONGO_URI, PORT } from './config';
+import app from './app';
+
+const bootstrapServer = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    app.listen(PORT);
+    console.log(`Server started on port: ${PORT}`);
+  } catch (error) {
+    throw new Error('Mongo Connection failed');
+  }
+};
+
+bootstrapServer();
