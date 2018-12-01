5115c7ef568d20c420fddae8d97ea005516b9c4b
Abdullah Kady <abdullahkady@gmail.com>
2018-12-01 12:43:34 +0200

    Add initial notifier code

---
+++ notifier/src/index.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { UNAUTHORIZED } from 'http-status';
+import { MONGO_URI, mongoConnectionOptions } from './config';
+import User from './user';
+
+const checkUsersGrades = async (user) => {
+  try {
+    // Get courses
+  } catch (error) {
+    if (error.status === UNAUTHORIZED) {
+      // TODO: Remove the user from the database since his password expired
+    }
+  }
+};
+
+const handleReadyUsers = async () => {
+  const users = await User.find({
+    nextCheckTimestamp: {
+      $lte: new Date(),
+    },
+  });
+
+  users.forEach(checkUsersGrades);
+};
+
+const bootApplication = async () => {
+  try {
+    await mongoose.connect(
+      MONGO_URI,
+      mongoConnectionOptions,
+    );
+
+    handleReadyUsers();
+    setInterval(handleReadyUsers, 15000);
+    console.log('Started application successfully');
+  } catch (error) {
+    throw error;
+  }
+};
+
+bootApplication();
