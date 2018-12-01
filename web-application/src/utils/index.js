0a7afd3fd75222a0c207619bbab60aac16843163
Abdullah Kady <abdullahkady@gmail.com>
2018-12-01 20:35:24 +0200

    Add email validation utility

--- web-application/src/utils/index.js
+++ web-application/src/utils/index.js
@@ -1,6 +1,12 @@
 import crypto from 'crypto';
 import { ENCRYPTION_SECRET } from '../config';
 
+const testEmail = (email) => {
+  // eslint-disable-next-line
+  const RE = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return RE.test(String(email).toLowerCase());
+};
+
 const encrypt = (text) => {
   const cipher = crypto.createCipher('aes-256-cbc', ENCRYPTION_SECRET);
   let crypted = cipher.update(text, 'utf8', 'hex');
@@ -8,4 +14,4 @@ const encrypt = (text) => {
   return crypted;
 };
 
-export { encrypt }; // eslint-disable-line
+export { encrypt, testEmail };
