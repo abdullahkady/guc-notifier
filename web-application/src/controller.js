dffbc2842db5db3f12c73d79bfbc25529d4fc256
Abdullah Kady <abdullahkady@gmail.com>
2019-01-07 22:01:11 +0200

    Add transcript on registeration

--- web-application/src/controller.js
+++ web-application/src/controller.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { CREATED, UNAUTHORIZED } from 'http-status';
-import { COURSEWORK_API_URI } from './config';
+import { COURSEWORK_API_URI, TRANSCRIPT_API_URI } from './config';
 import User from './models/user';
 
 const getCourses = async (username, password) => {
@@ -18,12 +18,29 @@ const getCourses = async (username, password) => {
   }
 };
 
+const getLatestTranscriptEntry = async (username, password) => {
+  try {
+    const { data } = await axios.post(TRANSCRIPT_API_URI, { username, password }, {});
+    return data.transcript.semesters.pop(); // Get the latest entry only
+  } catch ({ response }) {
+    const error = new Error();
+    const { status } = response;
+    if (status === UNAUTHORIZED) {
+      error.message = 'Invalid credentials';
+    }
+    error.status = status;
+    throw error;
+  }
+};
+
 const subscribeUser = async (req, res, next) => {
   const { username, password, email } = req.body;
 
-  let courses;
+  let coursework;
+  let transcript;
   try {
-    courses = await getCourses(username, password);
+    coursework = await getCourses(username, password);
+    transcript = await getLatestTranscriptEntry(username, password);
   } catch (err) {
     return next(err);
   }
@@ -33,7 +50,8 @@ const subscribeUser = async (req, res, next) => {
     password,
     email,
     latestGrades: {
-      coursework: courses,
+      coursework,
+      transcript,
     },
     nextCheckTimestamp: new Date(),
   };
